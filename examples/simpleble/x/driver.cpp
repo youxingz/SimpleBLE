a4f602cdc9ac9fbecac904dfa56c3528a27262ee
Youxing Zhou <youxingzeta@gmail.com>
2023-07-25 12:55:07 +0800

    fix windows build

--- examples/simpleble/x/driver.cpp
+++ examples/simpleble/x/driver.cpp
@@ -3,79 +3,85 @@
 
 #include "simpleble_c/simpleble.h"
 
+#if defined(__WIN32__)
+#define BLEX_EXPORT __declspec(dllexport)
+#else
+#define BLEX_EXPORT __declspec(dllexport)
+#endif
+
 // proxy func.
 #ifdef __cplusplus
 extern "C" {
 #endif
-typedef simpleble_err_t BlexErrorCode;
-typedef simpleble_adapter_t BlexAdapter;
-typedef simpleble_peripheral_t BlexPeripheral;
-typedef simpleble_address_type_t BlexAddressType;
-typedef simpleble_service_t BlexService;
-typedef simpleble_manufacturer_data_t BlexManufacturerData;
-typedef simpleble_uuid_t BlexUUID;
+BLEX_EXPORT typedef simpleble_err_t BlexErrorCode;
+BLEX_EXPORT typedef simpleble_adapter_t BlexAdapter;
+BLEX_EXPORT typedef simpleble_peripheral_t BlexPeripheral;
+BLEX_EXPORT typedef simpleble_address_type_t BlexAddressType;
+BLEX_EXPORT typedef simpleble_service_t BlexService;
+BLEX_EXPORT typedef simpleble_manufacturer_data_t BlexManufacturerData;
+BLEX_EXPORT typedef simpleble_uuid_t BlexUUID;
 
 // adapter.h
 
-bool    blexAdapterIsBluetoothEnabled() {
+BLEX_EXPORT bool    blexAdapterIsBluetoothEnabled() {
   return simpleble_adapter_is_bluetooth_enabled();
 }
 
-size_t  blexAdapterGetCount() {
+BLEX_EXPORT size_t  blexAdapterGetCount() {
   return simpleble_adapter_get_count();
 }
 
-BlexAdapter blexAdapterGetHandle(size_t index) {
+BLEX_EXPORT BlexAdapter blexAdapterGetHandle(size_t index) {
   return simpleble_adapter_get_handle(index);
 }
 
-void    blexAdapterReleaseHandle(BlexAdapter handle) {
+BLEX_EXPORT void    blexAdapterReleaseHandle(BlexAdapter handle) {
   simpleble_adapter_release_handle(handle);
 }
 
-char*   blexAdapterIdentifier(BlexAdapter handle) {
+BLEX_EXPORT char*   blexAdapterIdentifier(BlexAdapter handle) {
   return simpleble_adapter_identifier(handle);
 }
 
-char*   blexAdapterAddress(BlexAdapter handle) {
+BLEX_EXPORT char*   blexAdapterAddress(BlexAdapter handle) {
   return simpleble_adapter_address(handle);
 }
 
-BlexErrorCode blexAdapterScanStart(BlexAdapter handle) {
+BLEX_EXPORT BlexErrorCode blexAdapterScanStart(BlexAdapter handle) {
   return simpleble_adapter_scan_start(handle);
 }
 
-BlexErrorCode blexAdapterScanStop(BlexAdapter handle) {
+BLEX_EXPORT BlexErrorCode blexAdapterScanStop(BlexAdapter handle) {
   return simpleble_adapter_scan_stop(handle);
 }
 
-BlexErrorCode blexAdapterScanIsActive(BlexAdapter handle, bool * active) {
+BLEX_EXPORT BlexErrorCode blexAdapterScanIsActive(BlexAdapter handle, bool * active) {
   return simpleble_adapter_scan_is_active(handle, active);
 }
 
-BlexErrorCode blexAdapterScanFor(BlexAdapter handle, int timeout_ms) {
+BLEX_EXPORT BlexErrorCode blexAdapterScanFor(BlexAdapter handle, int timeout_ms) {
   return simpleble_adapter_scan_for(handle, timeout_ms);
 }
 
-size_t    blexAdapterScanGetResultsCount(BlexAdapter handle) {
+BLEX_EXPORT size_t    blexAdapterScanGetResultsCount(BlexAdapter handle) {
   return simpleble_adapter_scan_get_results_count(handle);
 }
 
-BlexPeripheral blexAdapterScanGetResultsHandle(BlexAdapter handle,
+BLEX_EXPORT BlexPeripheral blexAdapterScanGetResultsHandle(BlexAdapter handle,
                                                     size_t index) {
   return simpleble_adapter_scan_get_results_handle(handle, index);
 }
 
-size_t    blexAdapterGetPairedPeripheralsCount(BlexAdapter handle) {
+BLEX_EXPORT size_t    blexAdapterGetPairedPeripheralsCount(BlexAdapter handle) {
   return simpleble_adapter_get_paired_peripherals_count(handle);
 }
 
-BlexPeripheral blexAdapterGetPairedPeripheralsHandle(BlexAdapter handle,
+BLEX_EXPORT BlexPeripheral blexAdapterGetPairedPeripheralsHandle(BlexAdapter handle,
                                                           size_t index) {
   return simpleble_adapter_get_paired_peripherals_handle(handle, index);
 }
 
-BlexErrorCode blexAdapterSetCallbackOnScanStart(
+BLEX_EXPORT BlexErrorCode blexAdapterSetCallbackOnScanStart(
       BlexAdapter handle,
       void (*callback)(BlexAdapter adapter, void* userdata),
       void* userdata
@@ -83,7 +89,7 @@ BlexErrorCode blexAdapterSetCallbackOnScanStart(
   return simpleble_adapter_set_callback_on_scan_start(handle, callback, userdata);
 }
 
-BlexErrorCode blexAdapterSetCallbackOnScanStop(
+BLEX_EXPORT BlexErrorCode blexAdapterSetCallbackOnScanStop(
       BlexAdapter handle,
       void (*callback)(BlexAdapter adapter, void* userdata),
       void* userdata
@@ -91,7 +97,7 @@ BlexErrorCode blexAdapterSetCallbackOnScanStop(
   return simpleble_adapter_set_callback_on_scan_stop(handle, callback, userdata);
 }
 
-BlexErrorCode blexAdapterSetCallbackOnScanUpdated(
+BLEX_EXPORT BlexErrorCode blexAdapterSetCallbackOnScanUpdated(
     BlexAdapter handle,
     void (*callback)(BlexAdapter adapter, BlexPeripheral peripheral, void* userdata),
     void* userdata
@@ -99,7 +105,7 @@ BlexErrorCode blexAdapterSetCallbackOnScanUpdated(
   return simpleble_adapter_set_callback_on_scan_updated(handle, callback, userdata);
 }
 
-BlexErrorCode blexAdapterSetCallbackOnScanFound(
+BLEX_EXPORT BlexErrorCode blexAdapterSetCallbackOnScanFound(
     BlexAdapter handle,
     void (*callback)(BlexAdapter adapter, BlexPeripheral peripheral, void* userdata),
     void* userdata) {
@@ -108,79 +114,79 @@ BlexErrorCode blexAdapterSetCallbackOnScanFound(
 
 // peripheral.h
 
-void blexPeripheralReleaseHandle(BlexPeripheral handle) {
+BLEX_EXPORT void blexPeripheralReleaseHandle(BlexPeripheral handle) {
   simpleble_peripheral_release_handle(handle);
 }
 
-char* blexPeripheralIdentifier(BlexPeripheral handle) {
+BLEX_EXPORT char* blexPeripheralIdentifier(BlexPeripheral handle) {
   return simpleble_peripheral_identifier(handle);
 }
 
-char* blexPeripheralAddress(BlexPeripheral handle) {
+BLEX_EXPORT char* blexPeripheralAddress(BlexPeripheral handle) {
   return simpleble_peripheral_address(handle);
 }
 
-BlexAddressType blexPeripheralAddressType(BlexPeripheral handle) {
+BLEX_EXPORT BlexAddressType blexPeripheralAddressType(BlexPeripheral handle) {
   return simpleble_peripheral_address_type(handle);
 }
 
-int16_t   blexPeripheralRssi(BlexPeripheral handle) {
+BLEX_EXPORT int16_t   blexPeripheralRssi(BlexPeripheral handle) {
   return simpleble_peripheral_rssi(handle);
 }
 
-int16_t   blexPeripheralPower(BlexPeripheral handle) {
+BLEX_EXPORT int16_t   blexPeripheralPower(BlexPeripheral handle) {
   return simpleble_peripheral_tx_power(handle);
 }
 
-uint16_t  blexPeripheralMtu(BlexPeripheral handle) {
+BLEX_EXPORT uint16_t  blexPeripheralMtu(BlexPeripheral handle) {
   return simpleble_peripheral_mtu(handle);
 }
 
-BlexErrorCode blexPeripheralConnect(BlexPeripheral handle) {
+BLEX_EXPORT BlexErrorCode blexPeripheralConnect(BlexPeripheral handle) {
   return simpleble_peripheral_connect(handle);
 }
 
-BlexErrorCode blexPeripheralDisconnect(BlexPeripheral handle) {
+BLEX_EXPORT BlexErrorCode blexPeripheralDisconnect(BlexPeripheral handle) {
   return simpleble_peripheral_disconnect(handle);
 }
 
-BlexErrorCode blexPeripheralIsConnected(BlexPeripheral handle, bool* connected) {
+BLEX_EXPORT BlexErrorCode blexPeripheralIsConnected(BlexPeripheral handle, bool* connected) {
   return simpleble_peripheral_is_connected(handle, connected);
 }
 
-BlexErrorCode blexPeripheralIsConnectable(BlexPeripheral handle, bool* connectable) {
+BLEX_EXPORT BlexErrorCode blexPeripheralIsConnectable(BlexPeripheral handle, bool* connectable) {
   return simpleble_peripheral_is_connectable(handle, connectable);
 }
 
-BlexErrorCode blexPeripheralIsPaired(BlexPeripheral handle, bool* paired) {
+BLEX_EXPORT BlexErrorCode blexPeripheralIsPaired(BlexPeripheral handle, bool* paired) {
   return simpleble_peripheral_is_paired(handle, paired);
 }
 
-BlexErrorCode blexPeripheralUnpair(BlexPeripheral handle) {
+BLEX_EXPORT BlexErrorCode blexPeripheralUnpair(BlexPeripheral handle) {
   return simpleble_peripheral_unpair(handle);
 }
 
-size_t     blexPeripheralGetServicesCount(BlexPeripheral handle) {
+BLEX_EXPORT size_t     blexPeripheralGetServicesCount(BlexPeripheral handle) {
   return simpleble_peripheral_services_count(handle);
 }
 
-BlexErrorCode blexPeripheralGetServices(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralGetServices(BlexPeripheral handle,
                                                 size_t index,
                                           BlexService* services) {
   return simpleble_peripheral_services_get(handle, index, services);
 }
 
-size_t     blexPeripheralManufacturerDataCount(BlexPeripheral handle) {
+BLEX_EXPORT size_t     blexPeripheralManufacturerDataCount(BlexPeripheral handle) {
   return simpleble_peripheral_manufacturer_data_count(handle);
 }
 
-BlexErrorCode blexPeripheralManufacturerDataGet(
+BLEX_EXPORT BlexErrorCode blexPeripheralManufacturerDataGet(
     BlexPeripheral handle, size_t index, BlexManufacturerData* manufacturer_data
   ) {
   return simpleble_peripheral_manufacturer_data_get(handle, index, manufacturer_data);
 }
 
-BlexErrorCode blexPeripheralRead(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralRead(BlexPeripheral handle,
                                        BlexUUID service,
                                        BlexUUID characteristic,
                                       uint8_t** data,
@@ -188,7 +194,7 @@ BlexErrorCode blexPeripheralRead(BlexPeripheral handle,
   return simpleble_peripheral_read(handle, service, characteristic, data, data_length);
 }
 
-BlexErrorCode blexPeripheralWriteRequest(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralWriteRequest(BlexPeripheral handle,
                                                 BlexUUID service,
                                                 BlexUUID characteristic,
                                           const uint8_t* data,
@@ -196,7 +202,7 @@ BlexErrorCode blexPeripheralWriteRequest(BlexPeripheral handle,
   return simpleble_peripheral_write_request(handle, service, characteristic, data, data_length);
 }
 
-BlexErrorCode blexPeripheralWriteCommand(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralWriteCommand(BlexPeripheral handle,
                                                BlexUUID service,
                                                BlexUUID characteristic,
                                          const uint8_t* data,
@@ -204,7 +210,7 @@ BlexErrorCode blexPeripheralWriteCommand(BlexPeripheral handle,
   return simpleble_peripheral_write_command(handle, service, characteristic, data, data_length);
 }
 
-BlexErrorCode blexPeripheralNotify(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralNotify(BlexPeripheral handle,
                                          BlexUUID service,
                                          BlexUUID characteristic,
                                             void (*callback)(BlexUUID service, BlexUUID characteristic, const uint8_t* data, size_t data_length, void* userdata),
@@ -212,7 +218,7 @@ BlexErrorCode blexPeripheralNotify(BlexPeripheral handle,
   return simpleble_peripheral_notify(handle, service, characteristic, callback, userdata);
 }
 
-BlexErrorCode blexPeripheralIndicate(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralIndicate(BlexPeripheral handle,
                                            BlexUUID service,
                                            BlexUUID characteristic,
                                               void (*callback)(BlexUUID service, BlexUUID characteristic, const uint8_t* data, size_t data_length, void* userdata),
@@ -220,13 +226,13 @@ BlexErrorCode blexPeripheralIndicate(BlexPeripheral handle,
   return simpleble_peripheral_indicate(handle, service, characteristic, callback, userdata);
 }
 
-BlexErrorCode blexPeripheralUnsubscribe(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralUnsubscribe(BlexPeripheral handle,
                                               BlexUUID service,
                                               BlexUUID characteristic) {
   return simpleble_peripheral_unsubscribe(handle, service, characteristic);
 }
 
-BlexErrorCode blexPeripheralReadDescriptor(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralReadDescriptor(BlexPeripheral handle,
                                                  BlexUUID service,
                                                  BlexUUID characteristic,
                                                  BlexUUID descriptor,
@@ -235,7 +241,7 @@ BlexErrorCode blexPeripheralReadDescriptor(BlexPeripheral handle,
   return simpleble_peripheral_read_descriptor(handle, service, characteristic, descriptor, data, data_length);
 }
 
-BlexErrorCode blexPeripheralWriteDescriptor(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralWriteDescriptor(BlexPeripheral handle,
                                                   BlexUUID service,
                                                   BlexUUID characteristic,
                                                   BlexUUID descriptor,
@@ -244,13 +250,13 @@ BlexErrorCode blexPeripheralWriteDescriptor(BlexPeripheral handle,
   return simpleble_peripheral_write_descriptor(handle, service, characteristic, descriptor, data, data_length);
 }
 
-BlexErrorCode blexPeripheralSetCallbackOnConnected(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralSetCallbackOnConnected(BlexPeripheral handle,
                                                             void (*callback)(BlexPeripheral peripheral, void* userdata),
                                                             void* userdata) {
   return simpleble_peripheral_set_callback_on_connected(handle, callback, userdata);
 }
 
-BlexErrorCode blexPeripheralSetCallbackOnDisconnected(BlexPeripheral handle,
+BLEX_EXPORT BlexErrorCode blexPeripheralSetCallbackOnDisconnected(BlexPeripheral handle,
                                                                void (*callback)(BlexPeripheral peripheral, void* userdata),
                                                                void* userdata) {
   return simpleble_peripheral_set_callback_on_disconnected(handle, callback, userdata);
@@ -269,7 +275,7 @@ BlexErrorCode blexPeripheralSetCallbackOnDisconnected(BlexPeripheral handle,
 // }
 
 
-int main() {
+BLEX_EXPORT int main() {
   printf("Test BLEX module :: ");
   bool is_enabled = blexAdapterIsBluetoothEnabled();
   if (is_enabled) {
